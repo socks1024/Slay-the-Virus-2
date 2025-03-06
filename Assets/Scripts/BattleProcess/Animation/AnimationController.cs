68d47f77ca0f3fdde75175a49c449514d380582c
socks1024 <3619907017@qq.com>
2025-03-06 10:23:19 +0800

    Rearrangement

--- Assets/Scripts/BattleProcess/Animation/AnimationController.cs
+++ Assets/Scripts/BattleProcess/Animation/AnimationController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TurnControlButton : MonoBehaviour
+public class AnimationController : MonoBehaviour
 {
-    
+
 }
