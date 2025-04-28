f510035f01d59de13eb419201781810b96aaee2e
KassieRaita <2646955527@qq.com>
2025-04-28 20:01:18 +0800

    LevelRandom

--- Assets/Scripts/Base/Scan.cs
+++ Assets/Scripts/Base/Scan.cs
@@ -25,29 +25,66 @@ public class Scan : MonoBehaviour
         bool[] levelchosen = new bool[missionDatas.Length];
 
 
-        
-
-        for (int i = 0; i < RedDots.Length; i++)
+        bool AllClear = true;
+        for (int i = 0; i < levelclear.Length; i++)
         {
-           for(int k = 0; k < levelclear.Length; k++)
+            if (levelclear[i] == false)
             {
-                if (levelclear[k] == false && levelchosen[k]==false)
+                AllClear = false;
+            }
+        }
+
+        if (AllClear == false)
+        {
+            for (int i = 0; i < RedDots.Length; i++)
+            {
+                for (int k = 0; k < levelclear.Length; k++)
                 {
-                    int randpos = Random.Range(0, pos.Length);
-
-                    while (pos[randpos] == true)//重了就重row一下
+                    if (levelclear[k] == false && levelchosen[k] == false)
                     {
-                        randpos = Random.Range(0, pos.Length);
+                        int randpos = Random.Range(0, pos.Length);
+
+                        while (pos[randpos] == true)//重了就重row一下
+                        {
+                            randpos = Random.Range(0, pos.Length);
+                        }
+
+                        RedDots[i].transform.position = RedDotPos[randpos].position;
+                        RedDots[i].gameObject.GetComponent<EnterBattle>().missionData = missionDatas[k];
+                        RedDots[i].gameObject.SetActive(true);
+
+                        levelchosen[k] = true;
+                        pos[randpos] = true;
+                        break;
                     }
-
-                    RedDots[i].transform.position = RedDotPos[randpos].position;
-                    RedDots[i].gameObject.GetComponent<EnterBattle>().missionData = missionDatas[k];
-                    RedDots[i].gameObject.SetActive(true);
-
-                    levelchosen[k] = true;
-                    pos[randpos] = true;
-                    break;
                 }
+            }
+        }
+        else
+        {
+           
+            for (int i = 0; i < RedDots.Length; i++)
+            {
+                int randomlevel = Random.Range(0, missionDatas.Length);
+                while (levelchosen[randomlevel] == true)
+                {
+                    randomlevel = Random.Range(0, missionDatas.Length);
+                }
+
+                int randpos = Random.Range(0, pos.Length);
+
+                while (pos[randpos] == true)//重了就重row一下
+                {
+                    randpos = Random.Range(0, pos.Length);
+                }
+
+                levelchosen[randomlevel] = true;
+                pos[randpos] = true;
+
+                RedDots[i].transform.position = RedDotPos[randpos].position;
+
+                RedDots[i].gameObject.GetComponent<EnterBattle>().missionData = missionDatas[randomlevel];
+                RedDots[i].gameObject.SetActive(true);
             }
         }
     }
