a449039000aa4fa9f2049f732382cd9dd11f62cb
socks1024 <3619907017@qq.com>
2025-05-03 16:48:20 +0800

    Update EnterBattlePanel.cs

--- Assets/Scripts/Base/EnterBattlePanel.cs
+++ Assets/Scripts/Base/EnterBattlePanel.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using static UnityEditor.Progress;
+// using static UnityEditor.Progress;
 
 public class EnterBattlePanel : MonoBehaviour
 {
