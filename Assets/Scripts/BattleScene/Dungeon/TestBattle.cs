51a2c4907df976064e8ba6593fc80f52ebcb4a40
socks1024 <3619907017@qq.com>
2025-05-03 19:57:52 +0800

    Update TestBattle.cs

--- Assets/Scripts/BattleScene/Dungeon/TestBattle.cs
+++ Assets/Scripts/BattleScene/Dungeon/TestBattle.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Timers;
 using UnityEngine;
 
 public class TestBattle : MonoBehaviour
@@ -13,27 +14,37 @@ public class TestBattle : MonoBehaviour
 
     void Start()
     {
-        
+        TimersManager.SetTimer("StartBattle", 0.5f, StartBattle);
     }
 
     void Update()
     {
         if(Input.GetMouseButtonDown(0) && !startedBattle)
         {
+            StartBattle();
+        }
+    }
+
+    void StartBattle()
+    {
+        if (!SaveSystem.Instance.getSave().TutorialClear[2])
+        {
+            SaveSystem.Instance.getSave().TutorialClear[2] = true;
+
             DialogueManager.Instance.ShowDialoguePanel()
                 .AddDialogueEvent(DialogueManager.Instance.loader, new int[]{6,7,8,9,10,11,12,13,14,15,16,17,18,19})
                 .ShowNextDialogueEvent();
-
-            EnterDungeonInfo info = Messenger.enterDungeonInfo;
-
-            if (info.p_Cards == null || info.p_Cards.Count == 0) info.p_Cards = deck;
-            if (info.p_Relics == null) info.p_Relics = relics;
-            if (info.missionData == null) info.missionData = mission;
-
-            DungeonManager.Instance.StartAdventure(info);
-            startedBattle = true;
-
-            AudioManager.Instance.PlaySFX("BattleStart");
         }
+
+        EnterDungeonInfo info = Messenger.enterDungeonInfo;
+
+        if (info.p_Cards == null || info.p_Cards.Count == 0) info.p_Cards = deck;
+        if (info.p_Relics == null) info.p_Relics = relics;
+        if (info.missionData == null) info.missionData = mission;
+
+        DungeonManager.Instance.StartAdventure(info);
+        startedBattle = true;
+
+        AudioManager.Instance.PlaySFX("BattleStart");
     }
 }
